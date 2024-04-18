bbbeaa600cd9ed7b24eac5d5acfd77471ef55f2c
gustavo luiz <gugahb_10@hotmail.com>
2024-04-18 19:26:11 -0300

    Query with multiple criteria

--- src/main/java/com/gustavoluiz/projectmongo/resources/PostResource.java
+++ src/main/java/com/gustavoluiz/projectmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.gustavoluiz.projectmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,18 @@ public class PostResource {
 	public ResponseEntity<List<Post>>  findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
 		text = URL.decodeParam(text); // decodificação de url
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>>  fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text); // decodificação de url
+		Date min = URL.convertDate(minDate, new Date(0));
+		Date max = URL.convertDate(maxDate, new Date(0));
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
