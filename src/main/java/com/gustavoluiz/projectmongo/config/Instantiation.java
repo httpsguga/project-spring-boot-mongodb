3fe078cb4ff1faaef4ea310a92eafcb686c5abfc
gustavo luiz <gugahb_10@hotmail.com>
2024-04-18 18:13:31 -0300

    Adding comments to posts

--- src/main/java/com/gustavoluiz/projectmongo/config/Instantiation.java
+++ src/main/java/com/gustavoluiz/projectmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gustavoluiz.projectmongo.domain.Post;
 import com.gustavoluiz.projectmongo.domain.User;
 import com.gustavoluiz.projectmongo.dto.AuthorDTO;
+import com.gustavoluiz.projectmongo.dto.CommentDTO;
 import com.gustavoluiz.projectmongo.repository.PostRepository;
 import com.gustavoluiz.projectmongo.repository.UserRepository;
 
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post p1 = new Post(null, sdf.parse("17/04/2024"), "Partiu viagem", "Vou viajar para são paulo com o fabiano!", new AuthorDTO(maria));
 		Post p2 = new Post(null, sdf.parse("16/04/2024"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2024"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("13/03/2024"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha ótimo dia!", sdf.parse("25/04/2024"), new AuthorDTO(alex));
+
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2));
 		
 		maria.getPosts().addAll(Arrays.asList(p1, p2));
